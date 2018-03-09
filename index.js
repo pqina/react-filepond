270863058eb74c037465c8361f0ea994a075465f
pqina <info@pqina.nl>
2018-03-09 10:20:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/*
+ * FilePondPluginImageTransform 1.1.0
+ * Licensed under MIT, https://opensource.org/licenses/MIT
+ * Please visit https://pqina.nl/filepond for details.
+ */
+// test if file is of type image
 import React, { createElement } from 'react';
 
 // Import required methods and styles from the FilePond module, should not need anything else
@@ -12,84 +18,115 @@ const isSupported = supported();
 
 // returns file sources from the <File/> child objects
 const getFilesFromChildren = children =>
-    React.Children.map(
-        children,
-        child =>
-            child.props.type
-                ? Object.assign({}, child.props)
-                : child.props.source
-    );
+  React.Children.map(
+    children,
+    child => {
+        if (child.props.type) {
+            return {
+                source: child.props.source,
+                options: {
+                    type: child.props.type
+                }
+            }
+        }
+        else {
+            return child.props.source;
+        }
+    }
+  );
+
+// filtered methods
+const filteredMethods = [
+  'setOptions',
+  'on',
+  'off',
+  'onOnce',
+  'appendTo',
+  'insertAfter',
+  'insertBefore',
+  'isAttachedTo',
+  'replaceElement',
+  'restoreElement',
+  'destroy'
+];
 
 // The React <FilePond/> wrapper
 export class FilePond extends React.Component {
-    // Will setup FilePond instance when mounted
-    componentDidMount() {
-        // exit here if not supported
-        if (!isSupported) {
-            return;
-        }
-
-        // Get files from children (either as array of objects or sources)
-        const files = getFilesFromChildren(this.props.children);
-
-        // Create our pond
-        this.pond = create(this.element, Object.assign({}, this.props, files));
+  // Will setup FilePond instance when mounted
+  componentDidMount() {
+    // exit here if not supported
+    if (!isSupported) {
+      return;
     }
 
-    // Will clean up FilePond instance when unmounted
-    componentWillUnmount() {
-        // exit when no pond defined
-        if (!this.pond) {
-            return;
-        }
+    // Get files from children (either as array of objects or sources)
+    const files = getFilesFromChildren(this.props.children);
 
-        this.pond.destroy();
+    // Create our pond
+    this._pond = create(this._element, Object.assign({}, this.props, files));
+
+    // Reference pond methods to FilePond component instance
+    Object.keys(this._pond)
+      .filter(key => !filteredMethods.includes(key))
+      .forEach((key, index) => {
+        this[key] = this._pond[key];
+      });
+  }
+
+  // Will clean up FilePond instance when unmounted
+  componentWillUnmount() {
+    // exit when no pond defined
+    if (!this._pond) {
+      return;
     }
 
-    // Something changed
-    componentDidUpdate(prevProps) {
-        // exit when no pond defined
-        if (!this.pond) {
-            return;
-        }
+    this._pond.destroy();
+  }
 
-        const options = Object.assign({}, this.props);
-
-        // file list has changed
-        if (prevProps.children !== this.props.children) {
-            options.files = getFilesFromChildren(this.props.children);
-        }
-
-        this.pond.setOptions(options);
+  // Something changed
+  componentDidUpdate(prevProps) {
+    // exit when no pond defined
+    if (!this._pond) {
+      return;
     }
 
-    // Renders basic element hook for FilePond to attach to
-    render() {
-        const {
-            id,
-            name,
-            className,
-            allowMultiple,
-            required,
-            captureMethod,
-            acceptedFileTypes
-        } = this.props;
-        return createElement(
-            'div',
-            { className: 'filepond--wrapper' },
-            createElement('input', {
-                type: 'file',
-                name,
-                id,
-                accept: acceptedFileTypes,
-                multiple: allowMultiple,
-                required: required,
-                className: className,
-                capture: captureMethod,
-                ref: element => (this.element = element)
-            })
-        );
+    const options = Object.assign({}, this.props);
+
+    // file list has changed
+    if (prevProps.children !== this.props.children) {
+      options.files = getFilesFromChildren(this.props.children);
     }
+
+    this._pond.setOptions(options);
+  }
+
+  // Renders basic element hook for FilePond to attach to
+  render() {
+    const {
+      id,
+      name,
+      className,
+      allowMultiple,
+      required,
+      captureMethod,
+      acceptedFileTypes
+    } = this.props;
+    return createElement(
+      'div',
+      { className: 'filepond--wrapper' },
+      createElement('input', {
+        type: 'file',
+        name,
+        id,
+        accept: acceptedFileTypes,
+        multiple: allowMultiple,
+        required: required,
+        className: className,
+        capture: captureMethod,
+        ref: element => (this._element = element)
+      })
+    );
+  }
 }
 
 // <File/>, needs to be further extended with prop types
