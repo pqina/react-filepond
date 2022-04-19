4ffd3d0c78b98662877b39d76e592f6b036b89ba
Rik <rik@pqina.nl>
2022-04-19 16:21:09 +0200

    update example to v18

--- example/src/index.js
+++ example/src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// <18
+// import ReactDOM from 'react-dom';
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// 18
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
