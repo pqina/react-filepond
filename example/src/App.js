5502a76ab1fdd5a43fd28b2bca468470f8271040
iriza <rzgggg12@gmail.com>
2024-03-25 22:48:40 +0330

    fixed registerPlugin import path

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,8 @@
 import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
-import { FilePond, registerPlugin } from "./react-filepond";
+import { FilePond } from "./react-filepond";
 import "filepond/dist/filepond.min.css";
+import {registerPlugin} from "filepond"
 import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
